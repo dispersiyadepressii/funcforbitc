5067cdc7f7ebd5e4fbdada1f8f8a6824bc22fbcf
dispersiyadepressii <124277199+dispersiyadepressii@users.noreply.github.com>
2023-07-26 17:25:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import time
 import functions
-#i btc today in euro:
-price = 26444
+import request
+# btc today in euro:
+price = float(request.PriceOfBitc())
+nameoffile = 'new_register.txt'
+
 sum, firsttax = functions.SellWithoutTax()
 
 print(functions.Benefit(sum,firsttax,price))
 
 functions.AddLine()
+
+
+
